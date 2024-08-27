5a1c509c83388ec5c364dcee24d4f9c2ab0cbfc8
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-08-27 12:28:26 -0500

    added "Section" with description of React and List of benefits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Section title="What is React?">
+        <p>React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on components by Facebook Inc. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.</p>
+      </Section>
+
+      <Section title="Benefits of React">
+        <ul>
+          <li>Easy to learn and use</li>
+          <li>Reusability of code components</li>
+          <li>Readily available JavaScript Libraries</li>
+          <li>Active community with hundreds of custom libraries to explore</li>
+          <li>Works for both iOS and Android App development</li>
+          <li>Great for SEO</li>
+        </ul>
+      </Section>
     </div>
   );
 }
