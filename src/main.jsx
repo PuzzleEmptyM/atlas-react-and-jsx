c8336690a2cf83c085fb2caf37846eb00904a45a
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-08-27 09:59:25 -0500

    Initial commit for task 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
 import "./index.css";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// Render the App component to the root element
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
